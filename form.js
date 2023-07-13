c6e984f69a2eaa7e4bac5e313d0f8ef8f70ae499
Siddharth  Manmothe <128028270+FriendlinkSocial@users.noreply.github.com>
2023-07-13 16:01:26 +0530

    Update form.js

--- form.js
+++ form.js
@@ -43,6 +43,21 @@ auth.signOut();
 alert("SignOut Successfully from System");
 }
 
+function DeleteUser() {
+    const user = firebase.auth().currentUser;
+ 
+    user
+        .delete()
+        .then(() => {
+            // User deleted.
+            console.log("User Account Deleted Successful");
+        })
+        .catch((error) => {
+            // An error occurred
+            // ...
+        });
+}
+
 // Active user to homepage
 firebase.auth().onAuthStateChanged((user) => {
 if (user) {
