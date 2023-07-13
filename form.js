37058f9485eb7d6edb6e229bd15a545ff641badc
Siddharth  Manmothe <128028270+FriendlinkSocial@users.noreply.github.com>
2023-07-13 16:09:32 +0530

    Update form.js

--- form.js
+++ form.js
@@ -43,20 +43,144 @@ auth.signOut();
 alert("SignOut Successfully from System");
 }
 
-function DeleteUser() {
-    const user = firebase.auth().currentUser;
- 
-    user
-        .delete()
-        .then(() => {
-            // User deleted.
-            console.log("User Account Deleted Successful");
-        })
-        .catch((error) => {
-            // An error occurred
-            // ...
-        });
+function deleteUser() {
+    const deleteUserList = (collectionName, documentId) => {
+        return firebase.firestore().collection(collectionName).doc(documentId).delete();
+    };
+
+    const deleteStorageFile = (storagePath) => {
+        const storageRef = firebase.storage().ref(storagePath);
+        return storageRef.delete();
+    };
+
+    const deleteCurrentUser = () => {
+        const user = firebase.auth().currentUser;
+        return user.delete();
+    };
+
+    const deleteFollowingList = () => {
+        const followingList = [];
+        const query = firebase.firestore().collection("f@*aDe12")
+            .doc(firebaseUser.uid)
+            .collection("F");
+
+        return query.get()
+            .then((snapshot) => {
+                snapshot.forEach((documentSnapshot) => {
+                    followingList.push(documentSnapshot.id);
+                });
+
+                const deletePromises = followingList.map((friendId) => {
+                    return deleteUserList("f@*aDe12", friendId);
+                });
+
+                return Promise.all(deletePromises);
+            });
+    };
+
+    const deleteReqList = () => {
+        const reqList = [];
+        const query = firebase.firestore().collection("rZ89&*DE")
+            .doc(firebaseUser.uid)
+            .collection("F");
+
+        return query.get()
+            .then((snapshot) => {
+                snapshot.forEach((documentSnapshot) => {
+                    reqList.push(documentSnapshot.id);
+                });
+
+                const deletePromises = reqList.map((reqId) => {
+                    return deleteUserList("rZ89&*DE", reqId);
+                });
+
+                return Promise.all(deletePromises);
+            });
+    };
+
+    const deletePendingReqList = () => {
+        const pendingList = [];
+        const query = firebase.firestore().collection("rZ89&*DE")
+            .doc(firebaseUser.uid)
+            .collection("P");
+
+        return query.get()
+            .then((snapshot) => {
+                snapshot.forEach((documentSnapshot) => {
+                    pendingList.push(documentSnapshot.id);
+                });
+
+                const deletePromises = pendingList.map((reqId) => {
+                    return deleteUserList("rZ89&*DE", reqId);
+                });
+
+                return Promise.all(deletePromises);
+            });
+    };
+
+    const deleteUserId = () => {
+        const userIdRef = firebase.firestore().collection("U34dlo@%").doc(firebaseUser.uid);
+
+        return userIdRef.get()
+            .then((documentSnapshot) => {
+                if (documentSnapshot.exists) {
+                    const username = documentSnapshot.get("u");
+                    const usernameRef = firebase.firestore().collection("Us789!z#").doc(username);
+
+                    return usernameRef.delete()
+                        .then(() => {
+                            const userRef = firebase.firestore().collection("U34dlo@%").doc(firebaseUser.uid);
+                            return userRef.delete();
+                        })
+                        .then(() => deleteStorage());
+                } else {
+                    return deleteStorage();
+                }
+            });
+    };
+
+    const deleteStorage = () => {
+        const deleteStorageFilePromises = [
+            deleteStorageFile(`c7689/${firebaseUser.uid}/c`),
+            ...followingList.map((reactedList) => deleteStorageFile(`c7689/${reactedList}/c`)),
+            deleteStorageFile(`u43a2/${firebaseUser.uid}/c7689`)
+        ];
+
+        return Promise.all(deleteStorageFilePromises)
+            .then(() => finalStep());
+    };
+
+    const finalStep = () => {
+        const user = firebase.auth().currentUser;
+
+        if (user) {
+            return user.delete()
+                .then(() => {
+                    console.log("Account Deleted Successfully");
+                })
+                .catch((error) => {
+                    console.log("Account Deletion Failed", error);
+                });
+        } else {
+            console.log("User is not logged in");
+        }
+    };
+
+    const firebaseUser = firebase.auth().currentUser;
+
+    if (firebaseUser) {
+        checkFollowing()
+            .then(() => checkReq())
+            .then(() => checkPending())
+            .then(() => deleteSeenBy())
+            .catch((error) => {
+                console.log("An error occurred", error);
+            });
+    } else {
+        console.log("User is not logged in");
+    }
 }
+
 
 // Active user to homepage
 firebase.auth().onAuthStateChanged((user) => {
