170fd80bc1b43db2d1f252ccf198956b71cdbc1e
Siddharth  Manmothe <128028270+FriendlinkSocial@users.noreply.github.com>
2023-07-13 20:23:14 +0530

    Update form.js

--- form.js
+++ form.js
@@ -42,16 +42,17 @@ alert("SignOut Successfully from System");
 async function deleteAccount() {
   var user = firebase.auth().currentUser;
   if (user) {
-    let followingList = [];
-    let reqList = [];
-    let pendingList = [];
+    var followingList = [];
+    var reqList = [];
+    var pendingList = [];
     alert("Fetching user data...");
-    
+    try {
       // Fetch following list
-      var followingRef = db.collection("f@*aDe12").doc(user.uid).collection("F").get();
+      var followingRef = await db.collection("f@*aDe12").doc(user.uid).collection("F").get();
       followingRef.forEach(function (doc) {
         var data = doc.data();
         followingList.push(data);
+	        alert(listArray.array);
 	        alert("Inside Try");
       });
       console.log(followingList);
@@ -64,7 +65,6 @@ async function deleteAccount() {
         reqList.push(data);
       });
       console.log(reqList);
-
       alert("Fetching pending requests...");
       // Fetch pending requests
       var pendingRef = await db.collection("rZ89&*DE").doc(user.uid).collection("P").get();
@@ -73,43 +73,36 @@ async function deleteAccount() {
         pendingList.push(data);
       });
       console.log(pendingList);
-
       alert("Deleting user data...");
       // Delete user data
       await db.collection("C99!2cz$").doc(user.uid).delete();
-
       alert("Deleting seenBy document...");
       // Delete seenBy document
       await db.collection("S45!dc&*").doc(user.uid).collection("F").doc(user.uid).delete();
-
       alert("Deleting seenBy list...");
       // Delete seenBy list
       const seenBySnapshot = await db.collection("S45!dc&*").doc(user.uid).collection("F").get();
       seenBySnapshot.forEach(async function (doc) {
         await doc.ref.delete();
       });
-
       alert("Deleting friend list...");
       // Delete friend list
       const friendListSnapshot = await db.collection("f@*aDe12").doc(user.uid).collection("F").get();
       friendListSnapshot.forEach(async function (doc) {
         await doc.ref.delete();
       });
-
       alert("Deleting friend requests...");
       // Delete friend requests
       const friendReqSnapshot = await db.collection("rZ89&*DE").doc(user.uid).collection("F").get();
       friendReqSnapshot.forEach(async function (doc) {
         await doc.ref.delete();
       });
-
       alert("Deleting pending requests...");
       // Delete pending requests
       const pendingReqSnapshot = await db.collection("rZ89&*DE").doc(user.uid).collection("P").get();
       pendingReqSnapshot.forEach(async function (doc) {
         await doc.ref.delete();
       });
-
       alert("Deleting user document and username...");
       // Delete user document and username
       const userDoc = await db.collection("U34dlo@%").doc(user.uid).get();
@@ -117,37 +110,34 @@ async function deleteAccount() {
         var username = userDoc.data().u;
         await db.collection("Us789!z#").doc(username).delete();
         await db.collection("U34dlo@%").doc(user.uid).delete();
-
         alert("Deleting user storage...");
         // Delete user storage
         var storageRef = storage.ref("c7689/" + user.uid + "/c");
         await storageRef.delete();
-
         alert("Deleting friends' storage...");
         // Delete friends' storage
         for (const friendId of followingList) {
           var friendStorageRef = storage.ref("c7689/" + friendId + "/c");
           await friendStorageRef.delete();
         }
-
         alert("Deleting profile storage...");
         // Delete profile storage
         var profileStorageRef = storage.ref("u43a2/" + user.uid + "/c7689");
         await profileStorageRef.delete();
-
         alert("Deleting user account...");
         // Delete user account
         await user.delete();
-
         finishAccountDeletion();
       } else {
         finishAccountDeletion();
       }
+    } catch (error) {
+      console.error("Error deleting user account:", error);
+    }
   } else {
     console.log("No user found.");
   }
 }
-
 
 
 // Additional steps after account deletion
