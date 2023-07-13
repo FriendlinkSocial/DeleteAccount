cd842ebe58a3bfab89455eba257814491a93adba
Siddharth  Manmothe <128028270+FriendlinkSocial@users.noreply.github.com>
2023-07-13 18:06:53 +0530

    Update form.js

--- form.js
+++ form.js
@@ -10,17 +10,6 @@ appId: "1:638805104947:android:05dd31296f5b3aeef10646",
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
-// Signup function
-function signUp() {
-var email = document.getElementById("email");
-var password = document.getElementById("password");
-const promise = auth.createUserWithEmailAndPassword(
-	email.value,
-	password.value
-);
-promise.catch((e) => alert(e.message));
-alert("SignUp Successfully");
-}
 
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
@@ -31,7 +20,6 @@ firebase.auth().onAuthStateChanged(function(user) {
   }
 });
 
-// SignIN function
 function signIn() {
 var email = document.getElementById("email");
 var password = document.getElementById("password");
@@ -39,11 +27,14 @@ const promise = auth.signInWithEmailAndPassword(
 			email.value, password.value);
 promise.catch((e) => alert(e.message));
 }
+
 // SignOut
 function signOut() {
 auth.signOut();
 alert("SignOut Successfully from System");
 }
+
+
 // Delete user account
 function deleteAccount() {
   var user = firebase.auth().currentUser;
